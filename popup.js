fb65a24648a83fff854330172d819a63b5ff3373
Evan Lynch <eflynch@mit.edu>
2014-05-13 21:38:51 -0400

    Made a lot of changes without commiting anything yaygit add --all :/

--- popup.js
+++ popup.js
@@ -1,15 +1,49 @@
 window.onload = function(){
-  document.getElementById('searchButton').onclick = searchText;
+  document.getElementById('searchText').onkeyup = searchText;
+  document.onkeyup = controls;
 };
-function searchText(){
-  var search = document.getElementById('searchText').value;
-  if(search){
-    chrome.tabs.executeScript({file:"search.js"});
-    chrome.tabs.executeScript({file:"fuse.js"});
 
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        chrome.tabs.sendMessage(tabs[0].id, {method:"search",searchText:search});
-    });
-    //chrome.tabs.sendMessage({method:'search',searchText:search});
-  }
+var state = 0;
+function controls(e){
+    if (e.which == 27){
+        window.close();
+    }
+    if (state == 0){
+        if (e.which == 13){
+            chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+                chrome.tabs.sendMessage(tabs[0].id, {method:"go"});
+                window.close();
+            });
+        } else if (e.which == 18){
+           state = 1; 
+           document.getElementById('searchText').disabled = true;
+        }
+    } else {
+        if (e.which == 13){
+            chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+                chrome.tabs.sendMessage(tabs[0].id, {method:"go"});
+                window.close();
+            });
+        } else if (e.which == 18){
+            state = 0;
+            document.getElementById('searchText').disabled = false;
+            document.getElementById('searchText').focus();
+        } else if (e.which == 78){
+            chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+                chrome.tabs.sendMessage(tabs[0].id, {method:"next"});
+            });
+        } else if (e.which == 80){
+            chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+                chrome.tabs.sendMessage(tabs[0].id, {method:"prev"});
+            });
+        }
+    }
+}
+function searchText(e){
+    var search = document.getElementById('searchText').value;
+    if(search){
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+            chrome.tabs.sendMessage(tabs[0].id, {method:"search",searchText:search});
+        });
+    }
 }
--- search.js
+++ search.js
@@ -1,30 +1,58 @@
-// Copyright (c) 2012 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-chrome.runtime.onMessage.addListener(function(message,sender,sendResponse){
-    var links = [],l = document.links;
-    for(var i=0;i<l.length; i++){
-        links.push({id:i,text:l[i].text,url:l[i].href,link:l[i]});
-    }
-    //var links = [{
-    //    id: 1,
-    //    text: 'The Great Gatsby',
-    //    url: 'F. Scott. Fitzgerald'
-    //},{
-    //    id: 2,
-    //    text: 'The DaVinci Code',
-    //    url: 'Dan Brown'
-    //}];
+function focus_highlight(text){
+    text.innerHTML = "<span style='color: #000000; background-color: #FF8800;'>" + text.innerHTML + "</span>";
+}
 
-    var options = {
-        keys: ['text'],
-        id: 'id'
-    }
-    var f = new Fuse(links, options);
-    var results = f.search(message.searchText);
-    alert(results);
-    for(var i=0;i<results.length;i++){
-        var r = results[i];
-        l[r].innerHTML = "<span style='color: #000000;'>" + l[r].innerHTML + "</span>";
+function highlight(text){
+    text.innerHTML = "<span style='color: #000000; background-color: #FFFF00;'>" + text.innerHTML + "</span>";
+}
+
+function unhighlight(text){
+    text.innerHTML = text.getElementsByTagName("span")[0].innerHTML;
+}
+
+var results = [];
+var prevResults = [];
+var focus = 0;
+
+chrome.runtime.onMessage.addListener(function(message,sender,sendResponse){
+    if (message.method == "go"){
+        var l = document.links;
+        window.location.href = l[focus].href;
+    } else if  (message.method == "search"){
+        var links = [],l = document.links;
+        for(var i=0;i<l.length; i++){
+            links.push({id:i,text:l[i].text,url:l[i].href,link:l[i]});
+        }
+        var options = {
+            keys: ['text'],
+            id: 'id',
+            threshold: 0.4
+        }
+        var f = new Fuse(links, options);
+        results = f.search(message.searchText);
+
+        for(var i=0;i<prevResults.length;i++){
+            unhighlight(l[prevResults[i]]);
+        }
+        prevResults = results.slice(0);
+
+        for(var i=0;i<results.length;i++){
+            var r = results[i];
+            var focus_found = false;
+            if (r == focus){
+                focus_highlight(l[r]);
+                focus_found = true;
+            } else {
+                highlight(l[r]);
+            }
+            if (focus_found == false){
+                focus_found = true;
+                focus = results[0];
+                unhighlight(l[focus]);
+                focus_highlight(l[focus]);
+            }
+        }
     }
 });
+
+
